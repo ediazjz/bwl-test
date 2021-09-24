6bd0a66d79fcd3700da4160b6ea1617de866da79
ediazjz <ediazjz@outlook.com>
2021-09-24 05:20:47 -0500

    Add Dashboard Layout
    Fix main padding

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,10 +1,23 @@
+import Head from 'next/head'
+
 import { Layout } from "../components";
+import styles from '../styles/Dashboard.module.css'
 
 export default function Dashboard() {
   return (
     <Layout>
-      <div>
-        Dashboard Page
+      <Head>
+        <title>Dashboard | BWL Test</title>
+      </Head>
+
+      <div className={styles.grid}>
+        <div className={styles.weather}></div>
+        <div className={styles.selectedCountry}></div>
+        <div className={styles.pendingTasks}></div>
+        <div className={styles.completedTasks}></div>
+        <div className={styles.time}></div>
+        <div className={styles.timeZones}></div>
+        <div className={styles.countries}></div>
       </div>
     </Layout>
   )
