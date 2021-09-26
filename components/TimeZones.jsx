4684af62f78f4a541cccc70888a9a6563dc70be8
ediazjz <ediazjz@outlook.com>
2021-09-26 07:08:36 -0500

    Add TimeZones Component

---
+++ components/TimeZones.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react"
+
+import { ErrorCard, SkeletonLoader, useCountry, useTimeZone } from "."
+import styles from '../styles/Lists.module.css'
+
+export const TimeZones = () => {
+  const { currentCountry } = useCountry()
+  const { currentTimeZone, updateTimeZone } = useTimeZone()
+  const [timeZonesList, setTimeZonesList] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  const getTimeZones = async() => {
+    setError('')
+    setIsLoading(true)
+
+    let list
+
+    try {
+      list = await fetch(`http://api.timezonedb.com/v2.1/list-time-zone?key=${process.env.NEXT_PUBLIC_TIME_ZONE_API_KEY}&format=json&country=${currentCountry.code}`)
+        .then(response => {
+          return response.json()
+        })
+    }
+    catch {
+      setError(`No se pudieron conseguir las zonas horarias de ${currentCountry.name}.\nIntenta de nuevo recargando la página`)
+    }
+    finally {
+      setIsLoading(false)
+    }
+
+    setTimeZonesList(list.zones)
+  }
+
+  useEffect(() => {
+    getTimeZones()
+  }, [currentCountry])
+
+  const standardizeZoneName = (string) => {
+    const newString = string.split("/").pop().replace(/_/g, ' ')
+    return newString
+  }
+
+  if(isLoading) {
+    return <SkeletonLoader />
+  }
+  if(error) {
+    return <ErrorCard error={error} />
+  }
+
+  return (
+    <ul className={styles.timeZones}>
+      {timeZonesList?.map((zone, index) => {
+        return (
+          <li key={index} className={`${styles.item} ${currentTimeZone === standardizeZoneName(zone.zoneName) && styles.itemActive}`}>
+            <button className={`btn ${styles.button}`} onClick={() => updateTimeZone(standardizeZoneName(zone.zoneName))}>
+              {standardizeZoneName(zone.zoneName)}
+            </button>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
