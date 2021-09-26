491e64eb9d98bec9c450f029d8ff7f178906eb52
ediazjz <ediazjz@outlook.com>
2021-09-26 07:05:33 -0500

    Add Reusable Error Card

---
+++ components/ErrorCard.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/ErrorCard.module.css'
+
+export const ErrorCard = ({ error }) => {
+  return (
+    <div className={styles.container}>
+      <span className={styles.text}>{error}</span>
+    </div>
+  )
+}
