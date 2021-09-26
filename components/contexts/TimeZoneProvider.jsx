36f709b692adf308dff825569f4b8a107d94cf14
ediazjz <ediazjz@outlook.com>
2021-09-26 06:53:40 -0500

    Add TimeZoneProvider

---
+++ components/contexts/TimeZoneProvider.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react"
+
+const TimeZoneContext = createContext()
+export const useTimeZone = () => {
+  return useContext(TimeZoneContext)
+}
+
+export const TimeZoneProvider = ({ children }) => {
+  const [currentTimeZone, setCurrentTimeZone] = useState('Mexico City')
+
+  const updateTimeZone = (timeZone) => {
+    setCurrentTimeZone(timeZone)
+  }
+
+  return (
+    <TimeZoneContext.Provider value={{ currentTimeZone, updateTimeZone }}>
+      {children}
+    </TimeZoneContext.Provider>
+  )
+}
--- components/contexts/index.js
+++ components/contexts/index.js
@@ -1,2 +1,3 @@
 export { AuthProvider, useAuth } from './AuthProvider'
 export { CountryProvider, useCountry } from './CountryProvider'
+export { TimeZoneProvider, useTimeZone } from './TimeZoneProvider'
